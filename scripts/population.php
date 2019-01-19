76303355607b27a40e5fe0a542edb7551d13c231
Sebastien Klasa <klasa.sebastien@gmail.com>
2019-01-19 16:12:35 +0100

    Add population_cercle

--- scripts/population.php
+++ scripts/population.php
@@ -4,12 +4,12 @@ include_once("conversion.php");
 
 /**
  * Récupère la population du carré de 200m de côté dans lequel se trouve la coordonnée.
- * @param pdo La connection à la base de données.
+ * @param pdo La connexion à la base de données.
  * @param lon La longitude.
  * @param lat La latitude.
  * @return int La population du carré.
  */
-function getPopulation($pdo, $lon, $lat)
+function population_carre($pdo, $lon, $lat)
 {
     $pos = WGS84_to_ETRS89($lon, $lat);
 
@@ -21,6 +21,32 @@ function getPopulation($pdo, $lon, $lat)
     $select = $pdo->prepare("SELECT population FROM population WHERE e = ? AND n = ?;");
     $select->execute([$e, $n]);
     return $select->fetch()['population'];
+}
+
+/**
+ * Calcule la densité de population à une position géographique.
+ * @param pdo La connexion à la base de données.
+ * @param lon La longitude.
+ * @param lat La latitude.
+ * @return double La densité de population en hab/km2.
+*/
+function densite($pdo, $lon, $lat)
+{
+    return population_carre($pdo, $lon, $lat) / (0.2 * 0.2);
+}
+
+/**
+ * Calcule une estimation de la population dans un cercle.
+ * @param pdo La connexion à la base de données.
+ * @param lon La longitude du centre du cercle.
+ * @param lat La latitude du centre du cercle.
+ * @param rayon Le rayon du cercle en mètres.
+ * @return double Une estimation de la population dans le cercle.
+*/
+function population_cercle($pdo, $lon, $lat, $rayon)
+{
+    $rayon_km = $rayon / 1000;
+    return densite($pdo, $lon, $lat) * $rayon_km * $rayon_km;
 }
 
 ?>
