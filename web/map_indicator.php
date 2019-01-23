feea6c408c63200694cae6ed8af67c391f4d1cf7
dimitri mikhailoff <dmikhailof@gmail.com>
2019-01-23 14:54:43 +0100

    Modification des données de la map en dynamique

--- web/map_indicator.php
+++ web/map_indicator.php
@@ -12,68 +12,101 @@
   </head>
   <body>
   	<p id="indicator_result">
-  		L'indicateur pour votre adresse : 
   		<?php 
-  		if (isset($_POST['number']) && isset($_POST['street']) && isset($_POST['district']) && isset($_POST['radius']))
-	    {
+	  		if (isset($_POST['number']) && isset($_POST['street']) && isset($_POST['district']) && isset($_POST['radius']))
+		    {
 
-	        $fichier = 'test_formulaire.xml';
-			$xml = simplexml_load_file($fichier);
+		        $fichier = 'test_formulaire.xml';
+				$xml = simplexml_load_file($fichier);
 
-			$addexist = false;
+				$addexist = false;
 
-			foreach($xml as $address)
-			{
-				$attAddress = $address->attributes();
-
-				if(strtolower($attAddress->number) == strtolower($_POST['number']) 
-					&& strtolower($attAddress->street) == strtolower($_POST['street']) 
-					&& strtolower($attAddress->district) == strtolower($_POST['district']))
+				foreach($xml as $address)
 				{
-					$addexist = true;
+					$attAddress = $address->attributes();
 
-					foreach($address as $indicator)
+					if(strtolower($attAddress->number) == strtolower($_POST['number']) 
+						&& strtolower($attAddress->street) == strtolower($_POST['street']) 
+						&& strtolower($attAddress->district) == strtolower($_POST['district']))
 					{
-						$attindicator = $indicator->attributes();
-						if($attindicator->radius == $_POST['radius'])
+						$addexist = true;
+
+						foreach($address as $indicator)
 						{
-							echo $_POST['number'].", ".$_POST['street']." ".$_POST['district']." dans un rayon de ".$_POST['radius']."m est de ";
-							echo $indicator;
+							$attindicator = $indicator->attributes();
+							if($attindicator->radius == $_POST['radius'])
+							{
+								$place = intval(number_format(floatval($indicator),2) * 10);
+								echo "Pour votre adresse : ".$_POST['number'].", ".$_POST['street']." 750".$_POST['district']." PARIS, il existe environ ".$place." places pour 10 personnes.";
+
+								$longitude = $attAddress->longitude;
+								$latitude = $attAddress->latitude;
+
+								if($indicator < 0.25)
+								{
+									$color = 'red';
+									$fillColor = '#f03';
+								}
+								elseif ($indicator < 0.75) 
+								{
+									$color = 'orange';
+									$fillColor = '#FFA500';
+								}
+								elseif ($indicator >= 0.75) 
+								{
+									$color = 'green';
+									$fillColor = '#9ACD32';
+								}
+								 
+								if($_POST['radius'] == 100)
+								{
+									$zoom = 17;
+								}
+								elseif($_POST['radius'] == 200)
+								{
+									$zoom = 16;
+								}
+								elseif($_POST['radius'] == 500)
+								{
+									$zoom = 15;
+								}
+
+							}
 						}
 					}
 				}
-			}
 
-			if($addexist == false)
-			{
-				echo "il n'existe pas d'indicateur pour votre adresse.";
-			}
-	    }
-	    else
-	    {
-	        echo '<p>Les données fournies par le formulaire sont incomplètes.</p>';
-	    }
-  			
+				if($addexist == false)
+				{
+					echo "Il n'existe pas d'indicateur pour votre adresse.";
+				}
+		    }
+		    else
+		    {
+		        echo "<p>Les données fournies par le formulaire sont incomplètes. Impossible de calculer l'indicateur.</p>";
+		    }  			
   		?>
   	</p>
+	
 	<div id="mapid" style="height: 400px;"></div>
-
 	<script type="text/javascript">
-		var mymap = L.map('mapid').setView([48.8534,  2.3488], 15);
+		var mymap = L.map('mapid').setView([<?php echo $latitude ?>, <?php echo $longitude ?>], <?php echo $zoom ?>);
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     	axZoom: 18,
     	id: 'mapbox.streets',
     	accessToken: 'pk.eyJ1IjoiY3lyaWxkbHQiLCJhIjoiY2pyNm50a2JqMTNtejQzcDRmN2FiYzcwMSJ9.tuoKnKQ24S-vrWdnQliNeQ'
 		}).addTo(mymap);
-		var marker = L.marker([48.8534,  2.3488]).addTo(mymap);
-		var circle = L.circle([48.8534,  2.3488], {
-    	color: 'red',
-    	fillColor: '#f03',
-    	fillOpacity: 0.5,
-    	radius: 200
-		}).addTo(mymap);
+
+		var marker = L.marker([<?php echo $latitude ?>, <?php echo $longitude ?>]).addTo(mymap);
+		var circle = L.circle([<?php echo $latitude ?>, <?php echo $longitude ?>], {
+													color: <?php echo json_encode($color) ?>,
+											    	fillColor: <?php echo json_encode($fillColor) ?>,
+											    	fillOpacity: 0.5,
+											    	radius: <?php echo $_POST['radius'] ?>}).addTo(mymap);
 	</script>
+	
+
 	<input type="button" value="back" onClick="document.location.href = document.referrer" class="button_reset"/>
 	
 
